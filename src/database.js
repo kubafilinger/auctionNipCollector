39245429b805330b20ef1351c6e6c0d1893a105a
Unknown <kubafilinger@gmail.com>
2018-03-12 19:30:13 +0100

    Create schema for database

--- src/database.js
+++ src/database.js
@@ -12,10 +12,22 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 
-    let sql = "CREATE TABLE IF NOT EXISTS `frauder` (" +
+    let sql = "CREATE TABLE IF NOT EXISTS `seller` (" +
             "id INT NOT NULL AUTO_INCREMENT," +
-            "name VARCHAR(255)," +
-            "nip VARCHAR(255)," +
+            "address VARCHAR(255) NULL DEFAULT NULL," +
+            "registration_date DATETIME NULL DEFAULT NULL," +
+            "check_date DATETIME NULL DEFAULT NULL," +
+            "email VARCHAR(255) NULL DEFAULT NULL," +
+            "phone VARCHAR(255) NULL DEFAULT NULL," +
+            "company VARCHAR(255) NULL DEFAULT NULL," +
+            "krs VARCHAR(255) NULL DEFAULT NULL," +
+            "regon VARCHAR(255) NULL DEFAULT NULL," +
+            "www VARCHAR(255) NULL DEFAULT NULL," +
+            "created_at DATETIME NULL DEFAULT CURRENT_TIMESTAMP," +
+            "updated_at DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP," +
+            "mojepanstwo_url VARCHAR(255) NULL DEFAULT NULL," +
+            "allegro_username VARCHAR(255) NULL DEFAULT NULL," +
+            "nip VARCHAR(255) NULL DEFAULT NULL," +
             "PRIMARY KEY (`id`)" +
         ");"
     ;
