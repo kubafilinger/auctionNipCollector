332bf704b760b1e411c2387ad5a4ceee03ade13d
Ghost <jakubf.rekinysukcesu@gmail.com>
2018-04-30 15:49:54 +0200

    Refactor application - process management

--- src/sync.js
+++ src/sync.js
@@ -1,10 +1,13 @@
+const sequelize = require('./connection');
 var Product = require('./models/Product');
 var Seller = require('./models/Seller');
 
 Seller.sync({force: true}).then(() => {
     console.log('create table Seller')
-});
 
-Product.sync({force: true}).then(() => {
-    console.log('create table Product')
+    Product.sync({force: true}).then(() => {
+        console.log('create table Product')
+
+        sequelize.close();
+    });
 });
